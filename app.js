e1953fa2c55ed73b52ecdbd345086481361acf18
Devansh Kaneriya <devansh.kaneriya@kevit.io>
2023-07-31 16:57:12 +0530

    video 10

---
+++ app.js
@@ -0,0 +1,7 @@
+// const fs = require('fs')
+// fs.appendFileSync('notes.txt'," Devansh hello")
+// fs.writeFileSync('notes.txt','This file was created by Node.js')
+
+require('./utils')
+const name ='Devansh'
+console.log(name)
