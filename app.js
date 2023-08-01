809d209302dfda40870f57f056593f41d66cfc7f
Devansh Kaneriya <devansh.kaneriya@kevit.io>
2023-08-01 16:23:39 +0530

    Printing in color video 12

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const validator = require('validator')
+const chalk = require('chalk')
 const getNotes = require('./notes')
 
 const msg = getNotes()
@@ -6,3 +7,4 @@ console.log(msg)
 
 console.log(validator.isEmail('example.com'))
 console.log(validator.isURL('https://www.npmjs.com/package/validator'))
+console.log(chalk.green.inverse.bold('Success'))
