f44307eba2a25b8a140baacc3c0a366b35dd4d9f
Devansh Kaneriya <devansh.kaneriya@kevit.io>
2023-08-01 19:43:49 +0530

    getting user input video 15

--- app.js
+++ app.js
@@ -7,4 +7,15 @@ console.log(msg)
 
 console.log(validator.isEmail('example.com'))
 console.log(validator.isURL('https://www.npmjs.com/package/validator'))
-console.log(chalk.green.inverse.bold('Success'))
+console.log(chalk.green.inverse.bold('Success!'))
+console.log(chalk.blue.inverse.bold('Success!'))
+
+const command = process.argv[2] 
+
+console.log(process.argv)
+
+if(command === "add"){
+  console.log("Adding note  !")
+}else if(command === "remove"){
+  console.log("Removing note!")
+}
